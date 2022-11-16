3444f3f166115d29fcbb15738736feddbeb76ad8
fedechaves <federicochavesf@gmail.com>
2022-11-16 17:20:09 -0300

    todo list finished

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,13 @@
-import { FaTimes, FaTrash } from 'react-icons/fa'
+import { FaTrash } from 'react-icons/fa'
 
 const task = ({ task, onDelete, onToggle}) => {
   return (
-    <div className={`task ${task.reminder ? 'reminder' : ''}`} 
-    onDoubleClick={() => onToggle(task.id)}>
-      <h3>{task.text} <FaTrash style={{ cursor: 'pointer' }} 
-          onClick={() => onDelete(task.id)} />
+    <div className={`task ${task.reminder && 'reminder'}`} 
+    onDoubleClick={() => onToggle(task.id)}
+    >
+      <h3>{task.text}{' '}
+      <FaTrash style={{ cursor: 'pointer' }} 
+              onClick={() => onDelete(task.id)} />
       </h3>
       <p>{task.day}</p>
     </div>
